fb050a4e95b9457c687b6fa8f724dffb4ba9430e
Andrew Jones <pic16c7x@gmail.com>
2021-01-07 10:56:26 +0000

    Update newfact.rb

--- lib/facter/newfact.rb
+++ lib/facter/newfact.rb
@@ -1,9 +1,9 @@
 # hiera lookup "%{::newfact}.yaml"
 Facter.add('newfact') do
   setcode do
-    oldfact=Facter.value('oldfact')
-    oldfact.to_i
-    if oldfact.between?(100,900)
+    convertfact=Facter.value('oldfact')
+    convertfact.to_i
+    if convertfact.between?(100,900)
       newfact = '1100'
     end
     newfact.to_s
