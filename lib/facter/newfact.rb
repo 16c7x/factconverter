38077810d9f234f853144c841c0f67b11b0d09dc
Andrew Jones <pic16c7x@gmail.com>
2021-01-07 10:47:32 +0000

    Update newfact.rb

--- lib/facter/newfact.rb
+++ lib/facter/newfact.rb
@@ -3,6 +3,7 @@ Facter.add('newfact') do
   setcode do
     #oldfact=Facter.value('oldfact')
     oldfact = '101'
+    puts oldfact.to_i
     if oldfact.between?(100,900)
       newfact = '1100'
     end
